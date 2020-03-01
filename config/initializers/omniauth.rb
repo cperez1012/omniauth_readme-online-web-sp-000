c641f3264ad863ece2e629f688b689a19f423514
Christian Perez <perezchristian1012@gmail.com>
2020-03-01 02:54:15 +0000

    Automatically backed up by Learn

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+<<<<<<< HEAD
     provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
 end
+=======
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+end
+>>>>>>> 6e70220bba31ce27c49bff088c15282b250c4dae
